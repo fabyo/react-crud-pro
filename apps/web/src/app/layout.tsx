b7362e94db2879751f23bc795306bddb832af481
root <root@DESKTOP-MIRHNRT.localdomain>
2025-09-16 23:22:41 -0300

    fix(web): Força a atualização tipagem no RootLayout

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode // <-- Tipo CORRETO
+  children: React.ReactNode
 }) {
   return (
     <html lang="pt-BR">
